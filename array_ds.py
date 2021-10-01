ec1b1008366d540b6c670583c365b4cf15f73cd5
dev Sid <syvwgl@gmail.com>
2021-10-01 17:49:09 +0300

    Reverse array task completed

---
+++ array_ds.py
@@ -0,0 +1,28 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# https://www.hackerrank.com/challenges/arrays-ds/problem
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(array : list[int]) -> list[int] :
+    return [rev for rev in reversed(array)]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
