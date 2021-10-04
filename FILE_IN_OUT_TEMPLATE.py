747f1171f9484d71c8ca2cfdea5e6579914314a7
dev Sid <syvwgl@gmail.com>
2021-10-04 18:20:31 +0300

    template correction

--- FILE_IN_OUT_TEMPLATE.py
+++ FILE_IN_OUT_TEMPLATE.py
@@ -12,7 +12,7 @@ def funcName() :
 if __name__ == '__main__':
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
     frptr = open('./OUTPUT/IN', 'r')
-    fptr = open('./OUTPUT/OUT', 'w'))
+    fptr = open('./OUTPUT/OUT', 'w')
     
     lineNums = frptr.readline()
     lineTarget = frptr.readline()
