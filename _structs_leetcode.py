83e6e77612a4ae704abcdfb9050cb161a28c54ba
dev Sid <syvwgl@gmail.com>
2021-10-20 16:26:19 +0300

    added file with structures that are used in leetcode problems

---
+++ _structs_leetcode.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+# Realisation of custom structs that are needed in leetcode problems 
+
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def buildFromList(self, raw: list) -> TreeNode:
+        self.val = raw[0]
+        next_to_write = deque([self, self])
+        for node in raw[1:]:
+            place = next_to_write.popleft()   
+            if node:
+                new_node = TreeNode(node)
+                if place == next_to_write[0]:
+                    place.left = new_node
+                else:
+                    place.right = new_node
+                next_to_write.extend([new_node, new_node])
+
+        return self
+
+    def toList(self) -> list:
+        res = []
+        to_append = deque([self])
+        while len(to_append):
+            check = to_append.popleft()            
+            if check:
+                res.append(check.val)
+                to_append.append(check.left)
+                to_append.append(check.right)
+            else:
+                res.append(check) # None vall
+        
+        for i in range(len(res)-1, 0, -1):
+            if res[i]:
+                break
+        return res[:i+1]
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def buildFromList(self, raw: list) -> ListNode:
+        self.val = raw[0]
+        node = self
+        
+        for num in raw[1:]:
+            node.next = ListNode(num)
+            node = node.next
+        return self
+
+    def toList(self) -> list:
+        res = [self.val]
+        node = self
+        while node := node.next:
+            res.append(node.val)
+        return res
